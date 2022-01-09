9dcdbcac1cbece1e206e4d36695a74536051ce45
Christian Seifert <christian.seifert@tugraz.at>
2022-01-09 12:03:30 +0100

    Add support for more pages and 404 in case of wrong address

--- Server.js
+++ Server.js
@@ -4,8 +4,28 @@ const port = 3000
 
 app.use("/styles", express.static(__dirname + '/styles'));
 
-app.get('/', (req, res) => {
+app.get(['/', '/index.html'], (req, res) => {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(__dirname + '/about.html')
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+})
+
+app.get('/new.html', (req, res) => {
+    res.sendFile(__dirname + '/new.html')
+})
+
+app.get('/question', (req, res) => {
+    res.sendFile(__dirname + '/question.html')
+})
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/404.html')
 })
 
 app.listen(port, () => {
